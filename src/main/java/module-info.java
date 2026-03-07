16c655d4e982953ac24cde2e4885b867c6694211
Larry Mitchell <118214035+lmitchell137@users.noreply.github.com>
2026-03-06 23:13:55 -0500

    Update module descriptor for org.dynamisengine.core

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.dynamisengine.core {
+  requires java.logging;
+
+  exports org.dynamisengine.core.config;
+  exports org.dynamisengine.core.entity;
+  exports org.dynamisengine.core.event;
+  exports org.dynamisengine.core.exception;
+  exports org.dynamisengine.core.lifecycle;
+  exports org.dynamisengine.core.logging;
+  exports org.dynamisengine.core.resource;
+  exports org.dynamisengine.core.version;
+}
